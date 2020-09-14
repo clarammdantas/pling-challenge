356eeae18d4f3e583c36e11887b949f9fc0943b8
Clara Dantas <clara.moraesd@gmail.com>
2020-09-14 16:17:13 -0300

    This should be in the initial commit. Remember to squash

--- src/server.ts
+++ src/server.ts
@@ -7,18 +7,3 @@ db.connect();
 
 const app = new App();
 app.start();
-
-
-// import { app } from "./app";
-// import "../db";
-// import "./models/address";
-//
-// const PORT = process.env.PORT || 3000;
-//
-// app.listen(PORT, (err) => {
-//     if (err) {
-//         return console.log(err);
-//     }
-//
-//     console.log("Listening on port: " + PORT);
-// });
