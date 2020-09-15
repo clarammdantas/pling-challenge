c1638f6cf17f577360e51b682877db4eaa054e52
Clara Dantas <clara.moraesd@gmail.com>
2020-09-15 16:22:17 -0300

    deploy: Transpile source code to make a new deploy.

--- dist/models/address.js
+++ dist/models/address.js
@@ -1,42 +1,53 @@
 "use strict";
-// import mongoose from "mongoose";
-// import isValidCep from "@brazilian-utils/is-valid-cep";
-//
-// const Schema = mongoose.Schema;
-//
-// const Address = new Schema ({
-//     street: {
-//         type: String,
-//         required: true,
-//         max: 50
-//     },
-//     district: {
-//         type: String,
-//         required: true,
-//         max: 25
-//     },
-//     zipCode: {
-//         type: String,
-//         required: true,
-//         max: 8,
-//         validate: {
-//             validator: function(v) {
-//                 return isValidCep(v);
-//             },
-//             message: props => `${props.value} is not a valid CEP.`
-//         }
-//     },
-//     number: {
-//         type: Number,
-//         required: true,
-//         max: 99999  // Most countries use at most 5-digit numbers
-//         // to identify housing properties.
-//     },
-//     complement: {
-//         type: String,
-//         required: false,
-//         max: 50
-//     }
-// });
-//
-// exports.module = mongoose.model('Address', Address);
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const AddressSchema = new mongoose_1.Schema({
+    street: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return v.length <= 50;
+            },
+            message: props => `${props.value} the street name is too long.`
+        }
+    },
+    district: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return v.length <= 25;
+            },
+            message: props => `${props.value} the district name is too long.`
+        }
+    },
+    zipCode: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (v) {
+                const regex1 = /\d{8}/;
+                const regex2 = /\d{5}-\d{3}/;
+                return regex1.test(v) || regex2.test(v);
+            },
+            message: props => `${props.value} is not a valid zip code.`
+        }
+    },
+    number: {
+        type: Number,
+        required: true,
+        max: 99999
+    },
+    complement: {
+        type: String,
+        required: false,
+        validate: {
+            validator: function (v) {
+                return v.length <= 30;
+            },
+            message: props => `${props.value} the district name is too long.`
+        }
+    }
+});
+exports.default = mongoose_1.model('Adrress', AddressSchema);
